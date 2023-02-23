1659175a46ee9d1ed8c7d1094de64623f6e3460c
yudonggeun <ydong98@gmail.com>
2023-02-23 12:08:55 +0900

    모달 창 이슈 해결

--- src/component/modal.jsx
+++ src/component/modal.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function Modal(props) {
 
     const id = props.id;
     const alarm_text = props.text;
-    const functions = props.functions;
+    const request = props.functions;
+    const modal = useRef();
+
+    const close = () => {
+        modal.current.classList.add("hidden")
+    }
+
+    const functions = () => {
+        modal.current.classList.add("hidden")
+        request();
+    }
 
     return (
-        <div className="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-            id={id} tabIndex="-1" aria-labelledby={`${id}ModalLabel`} aria-hidden="true">
+        <div className="modal fixed hidden top-0 left-0 w-full h-full outline-none overflow-x-hidden overflow-y-auto bg-gray-900/50"
+            id={id} 
+            ref={modal}
+            tabIndex="-1"
+            aria-labelledby={`${id}ModalLabel`} 
+            aria-hidden="true">
             <div className="modal-dialog relative w-auto pointer-events-none">
                 <div
                     className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
@@ -24,7 +38,7 @@ function Modal(props) {
                     </div>
                     <div
                         className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-                        <button className="bg-red-500 hover:bg-red-600 text-white rounded-lg mx-2 my-3 p-1" data-bs-dismiss="modal">닫기</button>
+                        <button className="bg-red-500 hover:bg-red-600 text-white rounded-lg mx-2 my-3 p-1" onClick={() => close()}>닫기</button>
                         <button className="bg-green-500 hover:bg-green-600 text-white rounded-lg my-3 p-1" data-bs-dismiss="modal" onClick={() => functions()}>확인</button>
                     </div>
                 </div>
